d83fa4bb14f902051c697fc828c075e2f9341c43
chanz-pclimaco <pclimaco@chanzglobal.com>
2024-02-01 16:54:51 +0800

    From Model to Json

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,7 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Post extends Model
 {
@@ -15,7 +16,7 @@ class Post extends Model
         return $this->belongsTo(User::class);
     }
 
-    public function comments(): HasMany 
+    public function comments(): HasMany
     {
         return $this->hasMany(Comment::class);
     }
